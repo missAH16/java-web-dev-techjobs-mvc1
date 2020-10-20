1b8d5f89ccbd463802bd32a99895301125a65952
missAH16 <missahiatt@gmail.com>
2020-10-19 22:06:44 -0500

    commit

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.HashMap;
 
